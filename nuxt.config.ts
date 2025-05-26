e91dd061f6f46ddb98242082a7ef7c5a360e10b3
Tim Smith <tim.smith.hdg@gmail.com>
2025-05-26 11:16:11 -0700

    feat: zod env validation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineNuxtConfig } from "nuxt/config";
 
+import "./lib/env";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
