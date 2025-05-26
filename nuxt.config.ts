dbdb121678cb157970f0649c5f8b202943478578
Tim Smith <tim.smith.hdg@gmail.com>
2025-05-26 06:43:54 -0700

    feat: lint,lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
