d1dabcd4c8d25ac3f54fbec8f31c4a3bdadc1328
Tim Smith <gonad2019@gmail.com>
2025-05-26 17:32:35 -0700

    feat: Auth State (#56)
    Co-authored-by: Tim Smith <tim.smith.hdg@gmail.com>

---
+++ pages/error.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const route = useRoute();
+const error = route.query.error as string || "An unknown error occurred";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mx-auto mt-4 min-h-72 text-center flex flex-col items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>Oh Darn!! {{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-accent">
+      Go Back Home
+      <Icon name="tabler:arrow-left" size="24" />
+    </NuxtLink>
+  </div>
+</template>
