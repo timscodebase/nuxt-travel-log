159259bc183bfcdd27896c8cb4942a18c99157d2
Tim Smith <tim.smith.hdg@gmail.com>
2025-05-26 18:24:11 -0700

    feat: updated pro envs

--- lib/env.ts
+++ lib/env.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
+  AUTH_GITHUB_CLIENT_ID: z.string(),
+  AUTH_GITHUB_CLIENT_SECRET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
