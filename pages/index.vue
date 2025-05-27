3b89053742bf739242c62591663ab6620a221a01
Tim Smith <gonad2019@gmail.com>
2025-05-26 21:01:37 -0700

    Protected Routes (#59)
    * Tl 17 (#57)
    
    * feat: Auth State
    
    * feat: updated pro envs
    
    * feat: Github Auth
    
    * feat: Finished Auth
    
    ---------
    
    Co-authored-by: Tim Smith <tim.smith.hdg@gmail.com>
    
    * feat: Sign out icon (#58)
    
    Co-authored-by: Tim Smith <tim.smith.hdg@gmail.com>
    
    * feat: Protected Route
    
    ---------
    
    Co-authored-by: Tim Smith <tim.smith.hdg@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AuthButton from "~/components/auth-button.vue";
+const authStore = useAuthStore();
 </script>
 
 <template>
@@ -12,7 +12,10 @@ import AuthButton from "~/components/auth-button.vue";
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink v-else to="/dashboard" class="btn btn-accent">
+          Go to Dashboard
+        </NuxtLink>
       </div>
     </div>
   </div>
